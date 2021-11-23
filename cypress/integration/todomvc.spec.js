81eca595ca0c44a8730673f2cef650e5a7baccef
solarjoni <solarjoni@gmail.com>
2021-11-23 17:24:29 +0200

    interim

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -3,5 +3,16 @@
 it('should be able to add a new todo to the list', () => {
     //cy.visit('https://todomvc-app-for-testing.surge.sh?delay-new-todo=5000');
     cy.visit('https://todomvc-app-for-testing.surge.sh');
-    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}");
+    
+    cy.get('.new-todo', {timeout: 5000}).type("Clean room{enter}");
+    
+    cy.get('label').should('have.text', 'Clean room');
+    cy.get('.toggle').should('not.be.checked');
+
+   cy.get('.toggle').click();
+   cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+   
+   cy.contains('Clear').click();
+   cy.get('.todo-list').should('not.have.descendants', 'li');
+
 });
